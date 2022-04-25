53db11b272286d1b3839fa7f57f01bdd0b766ea2
Chen Lyu <chen.lyu.comling@gmail.com>
2022-04-25 15:04:41 +0200

    Now using re module to make sure the index is correct

--- file_to_index_text.py
+++ file_to_index_text.py
@@ -1,10 +1,11 @@
 import os
+import re
 from pathlib import Path
 
-bible_files = Path('./corpus').glob('*.txt')
+bible_files = Path('./bible_files').glob('*.txt')
 
-# os.makedirs('./bible_text')
-# os.makedirs('./bible_index')
+os.makedirs('./bible_text')
+os.makedirs('./bible_index')
 
 for bible in bible_files:
     directory, file_name = str(bible).split('/')
@@ -12,8 +13,18 @@ for bible in bible_files:
             open('./bible_text/' + file_name, 'a+') as text_file, \
             open('./bible_index/' + file_name, 'a+') as index_file:
         lines = source_file.readlines()
-        for line in lines[11:]:
-            index = line[:9]
-            content = line[9:]
-            text_file.write(content)
-            index_file.write(index + '\n')
+        save_flag = 0
+        for line in lines:
+            if save_flag:
+                try:fi
+                    index, content = line.split('\t')
+                    index = ''.join(re.findall(r'\d', index))
+                    text_file.write(content)
+                    index_file.write(index + '\n')
+                except ValueError as ve:
+                    print(file_name)
+                    print(line[:-1])
+                    print(ve)
+                    print('\n')
+            if 'notes' in line:
+                save_flag = 1
