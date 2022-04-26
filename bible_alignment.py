a87b06e674c59605cd4f05852fbfc6d0a31aebbc
Chen Lyu <chen.lyu.comling@gmail.com>
2022-04-27 00:01:39 +0200

    Final Version of Bible Alignment

--- bible_alignment.py
+++ bible_alignment.py
@@ -1,8 +1,9 @@
 import os
 import re
+import shutil
 from pathlib import Path
 
-align_tool_path = '/maceflomal/align.py'
+align_tool_path = '/Users/chen/Typology/maceflomal/'
 lan_to_file = {'deu': 'deu-x-bible-newworld.txt', 'eng': 'eng-x-bible-newworld2013.txt',
                'fra': 'fra-x-bible-newworld.txt', 'fin': 'fin-x-bible-newworld.txt',
                'ces': 'ces-x-bible-newworld.txt'}
@@ -29,47 +30,41 @@ def file_dic_generation(filename):
 
 for item in lan_to_file:
     # print(item)
+    item_alignment_dir = '/Users/chen/Typology/' + item
+    if not os.path.exists(item_alignment_dir):
+        os.mkdir(item_alignment_dir)
     with open('/Users/chen/Typology/bible_files/' + lan_to_file[item]) as source_file:
         source_file_dic = file_dic_generation(source_file)
     bible_files = Path('/Users/chen/Typology/bible_files').glob('*.txt')
     for bible in bible_files:
-        lan = str(bible).split('/')[1].split('-')[0]
-        if lan == item:
-            # print(str(bible) + ' is skipped!')
+        bible_name = str(bible).split('/')[-1]
+        bible_lan = bible_name.split('-')[0]
+        if bible_lan == item:
+            print(str(bible) + ' is skipped!')
             continue
         else:
-            file_name = item + '-' + str(bible).split('/')[1]
-            output_file_dir = '/Users/chen/Typology/alignment-' + file_name
+            gen_file_name = item + '-' + bible_name
+            gen_file_dir = '/Users/chen/Typology/' + gen_file_name
             with open(bible) as bible_file:
-                bible_dic = file_dic_generation(bible_file)
-            with open('./' + file_name, 'a+') as file:
-                source_language_file = os.path.abspath(file.name)
-                # a = 0
-                # b = 0
-                item_del = []
-                for bible_item in bible_dic:
-                    if bible_item in source_file_dic:
-                        file.write(source_file_dic[bible_item])
-                        # a += 1
+                tar_file_dic = file_dic_generation(bible_file)
+            with open(gen_file_dir, 'a+') as file:
+                verses_del = []
+                for verse in tar_file_dic:
+                    if verse in source_file_dic:
+                        file.write(source_file_dic[verse])
                     else:
-                        item_del.append(bible_item)
-                        # b += 1
-            for item in item_del:
-                bible_dic.pop(item)
-            temp_file_name = 'temp-' + file_name
-            with open('/Users/chen/Typology/' + temp_file_name, 'a+') as temp_file:
-                target_language_file = os.path.abspath(temp_file.name)
-                for item in bible_dic:
-                    temp_file.write(bible_dic[item])
-            # print('这里源文件有{}行没有数据！'.format(b))
-            print(len(source_file_dic))
-            print(len(bible_dic))
-            print(item_del)
-            print(source_language_file)
-            print(target_language_file)
-            print(output_file_dir)
-            # print('there are {} lines in total'.format(a))
-            os.system('python3 {} --overwrite -m 3 -s {} -t {} -f {}'.format(align_tool_path, source_language_file,
-                                                                             target_language_file, output_file_dir))
-            break
-    break
+                        verses_del.append(verse)
+            for verse_del in verses_del:
+                tar_file_dic.pop(verse_del)
+            temp_file_name = 'rev-' + bible_name
+            temp_file_dir = '/Users/chen/Typology/' + temp_file_name
+            with open(temp_file_dir, 'a+') as temp_file:
+                for verse in tar_file_dic:
+                    temp_file.write(tar_file_dic[verse])
+        s_file = shutil.move(gen_file_dir, align_tool_path + gen_file_name)
+        t_file = shutil.move(temp_file_dir, align_tool_path + temp_file_name)
+        align_result_file = item_alignment_dir + '/' + item + '-' + bible_lan + '.txt'
+        os.chdir('/Users/chen/Typology/maceflomal')
+        os.system('python3 {} --overwrite -m 3 -s {} -t {} -f {}'.format('align.py', s_file, t_file, align_result_file))
+        os.remove(s_file)
+        os.remove(t_file)
